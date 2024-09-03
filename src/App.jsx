633248f872b932d5dc9e52d30e164e22531fec1c
ShaikAbdulKhaliq <shaik.a@mobiusdtaas.ai>
2024-09-03 19:36:15 +0530

    vercel.json file added to solve routing problem

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <>
     <BrowserRouter>
     <Routes>
-      <Route  path="/" element={<Tilevarint1/>}/>
+      <Route  path="/"  element={<Tilevarint1/>}/>
       <Route path="/view1" element={<TileviewVariation2/>}/>
       <Route path="/view2" element={<Tileview/>}/>
     </Routes>
